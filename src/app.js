aba97e8e103cf99ecc79c1e869970820eabdd104
Sumit Kumar <sumitkumar67670@gmail.com>
2025-07-06 20:15:36 +0530

    controlers and routers define

--- src/app.js
+++ src/app.js
@@ -13,4 +13,12 @@ app.use(express.json({limit:"26kb"}))
 app.use(express.urlencoded({extended: true, limit:"16kb"}))
 app.use(express.static('public'))
 app.use(cookieParser())
+
+//import routes
+import {userRouter} from './routes/user.routes.js'
+
+//route declaration
+app.use('/api/v1/users', userRouter)    
+
+
 export {app}
